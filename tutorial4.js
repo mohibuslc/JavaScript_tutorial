6956a1e19f7218e6c8d2c89daab02310826be805
mohibuslc <mohib_uslc@outlook.com>
2022-06-10 16:40:41 +0100

    Tutorial4 Javascript with harry

---
+++ tutorial4.js
@@ -0,0 +1,77 @@
+
+// Data Type
+
+// Two type of Data Type 
+
+// 1. Primitive Data Type ; 2. Referance Data Type . 
+
+// => primitive Data Type are Cluster type of Data Type . 
+
+// Primitive Data type : 1. String . 2 Number . 3. Boolean . 4. Null . 5 Undefine 
+
+// 1, String Data type Example 
+
+let name = ' harry';
+
+//name = ' rahman'
+
+console.log(name);
+
+// 2. Number 
+
+let age = 45;
+
+console.log(" This is Age   " + age)
+
+// 3 . Boolean Data declear 
+
+const isDrive = false;
+
+console.log(isDrive)
+
+
+//  null value 
+
+let nullVar = null
+
+console.log('This is Null ' + (typeof nullVar));
+
+// 4. undefine value ; 
+
+
+let define = undefined;
+
+console.log(define)
+
+
+// Referance Data Type : 
+
+// Object : 
+
+let MyArry = [2, 3, 65, 21, 4, false, undefined, name = ' Kamal', age = 34];
+
+
+MyArry.push(99);
+
+console.log(" MayArray is " + MyArry);
+
+
+console.log(" Myarray wht type of Data : " + "  " + (typeof MyArry))
+
+// Array : 
+
+
+
+let Persone = [{ name: 'Jamal', job: ' WebDeveloper', Salary: '$350 perWeek' },
+
+
+{ name: 'kamal', job: 'Front end Developer', Salary: '$250perWeek' }
+
+
+]
+
+console.log(Persone)
+
+
+
+
